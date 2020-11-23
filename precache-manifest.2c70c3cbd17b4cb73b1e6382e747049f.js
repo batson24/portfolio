7baf81bfef8dac3e2cb3a9c5b881d7987b075314
Dominique Batson <batson24@Dominiques-MacBook-Pro.local>
2020-11-22 18:38:17 -0600

    Updates

---
+++ precache-manifest.2c70c3cbd17b4cb73b1e6382e747049f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "52983c469ac40a2b0da4",
+    "url": "/portfolio/static/css/main.505b28e3.chunk.css"
+  },
+  {
+    "revision": "52983c469ac40a2b0da4",
+    "url": "/portfolio/static/js/main.52983c46.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "98f3038f57f4db6897dd3a147fb603d1",
+    "url": "/portfolio/index.html"
+  }
+];
